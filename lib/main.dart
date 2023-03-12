bca8c00fecf1984a13e4989931616a4cc51a8e5c
shamiaz93 <63896432+shamiaz93@users.noreply.github.com>
2023-03-13 00:49:15 +0530

    Test commit for jenkins

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'AutoDoctor',
+      title: 'AutomobileDoctor',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
